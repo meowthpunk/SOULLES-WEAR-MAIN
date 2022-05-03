69236f6ce287a245fa9f8522f7057f03b050fc83
meowthpunk <decaydesigner@gmail.com>
2022-05-03 14:04:13 +0300

    make it better

--- static/js/main.js
+++ static/js/main.js
@@ -9,6 +9,7 @@
 var main_button = window.Telegram.WebApp.MainButton
 main_button.text = 'View ur cart'
 main_button.color = '#8b8bd0'
+console.log(window.Telegram.WebApp)
 window.Telegram.WebApp.ThemeParams.bg_color = #303035
 
 var cartable = true
