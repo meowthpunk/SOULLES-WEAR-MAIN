5b0d360b639368335e188776604f8231917a26e6
meowthpunk <decaydesigner@gmail.com>
2022-05-03 20:20:09 +0300

    make it better

--- static/js/main.js
+++ static/js/main.js
@@ -6,7 +6,8 @@
 
 // window.Telegram.WebApp.MainButton.onClick(pizdec())
 const urlParams = decodeURIComponent(window.Telegram.WebApp.initData)
-console.log(urlParams)
+let parsedParams = Object.fromEntries(new URLSearchParams(params))
+console.log(parsedParams)
 // let params = decodeURIComponent(…)
 // console.log(urlParams.get(id))
 window.Telegram.WebView.initParams.tgAppThemeParams = {"bg_color":"#8b8bd0","text_color":"#ffffff","hint_color":"#aaaaaa","link_color":"#8774e1","button_color":"#8774e1","button_text_color":"#ffffff"}
